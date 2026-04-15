7f8952997e4ebb0377f94779e4c170c66c3cece2
Firwin Lee <Firwin.Lee@gmail.com>
2026-04-14 20:13:19 -0700

    pls work

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
